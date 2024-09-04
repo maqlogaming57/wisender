fe80070e3d89dc4b02cfc24dbc59e9461d31787f
maqlogaming57 <trijy7@gmail.com>
2024-09-04 15:13:47 +0700

    test cors

--- core/server/app/config/app.js
+++ core/server/app/config/app.js
@@ -1,57 +1,75 @@
-const express = require('express');
-const { connectDatabase } = require('./database.js');
-const routerApi = require('../routes/api.js');
-const axios = require('axios');
-const { logger } = require('../lib/myf.velixs.js');
-const { loadCommands } = require('../config/commands.js');
-const cors = require('cors');
-const fs = require('fs');
-const https = require('https');
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+const https = require("https");
+const { logger } = require("../lib/myf.velixs.js");
+const { connectDatabase } = require("./database.js");
+const { loadCommands } = require("../config/commands.js");
+const routerApi = require("../routes/api.js");
 
 class app {
     constructor() {
         this.app = express();
         this.plugins();
         this.routes();
-        this.lv = '5.0.0';
+        this.lv = "5.0.0";
     }
 
     plugins() {
-        try{
-            axios.get('https://raw.githubusercontent.com/ilsyaa/lazy-version/master/walazy.json',{
-                httpsAgent: new https.Agent({
-                    rejectUnauthorized: false
-                })
-            }).then(res => {
+        // Check version
+        axios
+            .get(
+                "https://raw.githubusercontent.com/ilsyaa/lazy-version/master/walazy.json",
+                {
+                    httpsAgent: new https.Agent({
+                        rejectUnauthorized: false,
+                    }),
+                }
+            )
+            .then((res) => {
                 if (this.lv != res.data.version) {
-                    logger('warning', '[VERSION] NEW VERSION AVAILABLE: ' + res.data.version)
+                    logger(
+                        "warning",
+                        "[VERSION] NEW VERSION AVAILABLE: " + res.data.version
+                    );
                     if (res.data.required_update) {
-                        logger('warning', '[VERSION] REQUIRED UPDATE: ' + res.data.version)
-                        logger('warning', 'Contact Whatsapp Admin : 628574902345')
+                        logger(
+                            "warning",
+                            "[VERSION] REQUIRED UPDATE: " + res.data.version
+                        );
+                        logger(
+                            "warning",
+                            "Contact Whatsapp Admin : 628574902345"
+                        );
                         process.exit(0);
                     }
                 }
+            })
+            .catch((e) => {
+                logger("error", "[AXIOS] FAILED TO CHECK VERSION: " + e);
             });
-        }catch(e){
-          logger('error', '[AXIOS] FAILED TO CHECK VERSION: ' + e)
-        }
-        this.app.use(cors());
+
+        // CORS middleware
+        this.app.use(
+            cors({
+                origin: "*",
+                methods: "GET, POST, PUT, DELETE, OPTIONS",
+                allowedHeaders:
+                    "Origin, X-Requested-With, Content-Type, Accept",
+            })
+        );
+
+        // Body parsers
         this.app.use(express.json());
         this.app.use(express.urlencoded({ extended: true }));
-        this.app.use(express.json())
-        this.app.use((req, res, next) => {
-            res.header('Access-Control-Allow-Origin', '*');
-            res.set('Cache-Control', 'no-store');
-            res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-            res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-            next();
-        });
+
+        // Connect to database and load commands
         connectDatabase();
         loadCommands();
     }
 
     routes() {
-        this.app.use('/api', routerApi);
+        this.app.use("/api", routerApi);
     }
 }
 
